24f1e5c709bb5343ca8d72e6d3d434676df7be97
Axel Zahir Zarza Zurita <zahirzarza@gmail.com>
2025-03-13 17:19:40 -0600

    Update elJugador.java

--- proyecto/src/Jugadores/elJugador.java
+++ proyecto/src/Jugadores/elJugador.java
@@ -6,7 +6,7 @@ import Fichas.*;
  *
  *
  *@author Axel Zarza
- *@author Diego Varela
+ *@author Diego 
  *la interfaz "elJugador" establece un contrato que las clases deben seguir para representar a un jugador en un juego de dominó. Cada uno de estos métodos es esencial para la interacción de un jugador con las fichas y las reglas del juego.
   */
 public interface elJugador {
